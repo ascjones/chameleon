a294e6a95f68d6a0601907633c066bc607affda2
Andrew Jones <ascjones@gmail.com>
2020-12-15 16:57:24 +0000

    Binary encoded scale file

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,20 @@ mod tests {
     fn decode_metadata() {
         let root = env::var("CARGO_MANIFEST_DIR").unwrap_or(".".into());
         let root_path = path::Path::new(&root);
+
+        /* generate with
+            curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
+            | jq .result \
+            | cut -d '"' -f 2 \
+            | xxd -r -p > ./node-runtime.scale
+        */
         let path = root_path.join("node-runtime.scale");
 
         let mut file = fs::File::open(&path).expect("Error opening file");
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).unwrap();
 
-        if &bytes[..1] == b"Ox" {
-            let bytes = hex::decode(&bytes[2..]).unwrap();
-        }
-
         let metadata =
-            frame_metadata::RuntimeMetadataPrefixed::<CompactForm<String>>::decode(&mut &bytes[..]).unwrap();
+            frame_metadata::RuntimeMetadataPrefixed::<String>::decode(&mut &bytes[..]).unwrap();
     }
 }
