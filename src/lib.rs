089c87fc60492a22b83e9be566fc2f0971473089
Andrew Jones <ascjones@gmail.com>
2020-12-15 16:09:52 +0000

    Attempt decoding vnext metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
 #[cfg(test)]
 mod tests {
+    use std::{env, fs, io::Read, path};
+    use scale_info::form::CompactForm;
+    use codec::Decode;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn decode_metadata() {
+        let root = env::var("CARGO_MANIFEST_DIR").unwrap_or(".".into());
+        let root_path = path::Path::new(&root);
+        let path = root_path.join("node-runtime.scale");
+
+        let mut file = fs::File::open(&path).expect("Error opening file");
+        let mut bytes = Vec::new();
+        file.read_to_end(&mut bytes).unwrap();
+
+        if &bytes[..1] == b"Ox" {
+            let bytes = hex::decode(&bytes[2..]).unwrap();
+        }
+
+        let metadata =
+            frame_metadata::RuntimeMetadataPrefixed::<CompactForm<String>>::decode(&mut &bytes[..]).unwrap();
     }
 }
