fa1682dd04af15866d2879ac771b68ef7b870e6c
Andrew Jones <ascjones@gmail.com>
2020-12-15 17:24:10 +0000

    Create workspace and crates

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod tests {
     use std::{env, fs, io::Read, path};
-    use scale_info::form::CompactForm;
     use codec::Decode;
 
     #[test]
@@ -9,7 +8,7 @@ mod tests {
         let root = env::var("CARGO_MANIFEST_DIR").unwrap_or(".".into());
         let root_path = path::Path::new(&root);
 
-        /* generate with
+        /* generate with:
             curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
             | jq .result \
             | cut -d '"' -f 2 \
@@ -21,7 +20,7 @@ mod tests {
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).unwrap();
 
-        let metadata =
+        let _metadata =
             frame_metadata::RuntimeMetadataPrefixed::<String>::decode(&mut &bytes[..]).unwrap();
     }
 }
