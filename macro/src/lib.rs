f1c165c5fd9c0c87fb683d9af68a546793b72046
Andrew Jones <ascjones@gmail.com>
2020-12-20 13:41:03 +0000

    Wire up macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn generate_types(input: TokenStream) -> TokenStream {
+    let input = input.to_string();
+    let input = input.trim_matches('"');
+
+    let root = std::env::var("CARGO_MANIFEST_DIR").unwrap_or(".".into());
+    let root_path = std::path::Path::new(&root);
+    let path = root_path.join(input);
+
+    core::generate_runtime_types("runtime", path)
+        .expect("Runtime generation failed")
+        .into()
 }
