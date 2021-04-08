c3c84da323019370101f1ed4b921bca06c9d554a
Andrew Jones <ascjones@gmail.com>
2021-04-08 16:17:03 +0100

    Generate runtime outer event and add codec derives (#2)
    * Define runtime events enum and add subscribe_events module
    
    * Add example project for using CLI generated runtime module

---
+++ examples/codegen/src/main.rs
@@ -0,0 +1,15 @@
+//! Demonstrate use of CLI generated runtime module.
+//!
+//! Generated with:
+//! ```
+//! cargo run -p chameleon-cli \
+//!  | rustfmt --edition=2018 --emit=stdout \
+//!  > examples/codegen/src/substrate_node_runtime.rs
+//! ```
+mod substrate_node_runtime;
+
+fn main() {
+
+}
+
+
